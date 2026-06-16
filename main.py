a814019f9aa88db78a9c20e6ac0ee15e475e72e9
nozaireeeeeeeeee <nolanpenso9@gmail.com>
2026-06-16 17:44:08 +0200

    Update API endpoint for starting server

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def start_server(interaction: nextcord.Interaction):
     await interaction.response.defer()
 
     # Configuration de la requête vers MineStrator
-    url = f"https://api.minestrator.com/v1/servers/%7BSERVER_ID%7D/start"
+    url = f"https://panel.minestrator.com/api/client/servers/%7BSERVER_ID%7D/power?signal=start"
     headers = {
         "Authorization": f"Bearer {API_KEY}",
         "Content-Type": "application/json"
